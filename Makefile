542304d77ce095ed307e35136e791f31f344bdeb
Alicia <alicia.arenzana@gmail.com>
2022-11-30 10:04:38 +0100

    updated cog_tiler base so it can work

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 up:
 	docker-compose up --build database stac raster
+up-cogs:
+	docker-compose up --build cog-tiler
 
 down:
 	docker-compose down
--- services/cog_tiler/setup.py
+++ services/cog_tiler/setup.py
@@ -13,6 +13,7 @@ setup(
     name="cog_tiler",
     description=long_description,
     python_requires=">=3.7",
-    packages=find_namespace_packages(),
+    packages=find_namespace_packages(exclude=["tests*"]),
+    include_package_data=True,
     install_requires=inst_reqs,
 )
