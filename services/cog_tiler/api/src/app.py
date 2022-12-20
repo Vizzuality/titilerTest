423f47353038ad149aacf116496eb5edbe69d190
Biel Stela <biel.stela@gmail.com>
2022-12-20 16:50:08 +0100

    Adds cog profiling notebook

--- services/cog_tiler/api/src/app.py
+++ services/cog_tiler/api/src/app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from titiler.application.routers.cog import cog
+# from titiler.application.routers.cog import cog
+from titiler.core import TilerFactory
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 
 app = FastAPI()
-app.include_router(cog.router, prefix="/cog", tags=["Cloud Optimized GeoTIFF"])
+cog = TilerFactory()
+app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
 
 @app.get("/healthz", description="Health Check", tags=["Health Check"])
 def ping():
